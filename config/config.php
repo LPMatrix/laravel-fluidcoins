6b6ec09f38477230a0eb9244e1d5a2400ab2aa0a
Sanusi Mubaraq <mubaraqsanusi908@gmail.com>
2022-12-22 02:35:30 +0100

    support psr4

--- config/config.php
+++ config/config.php
@@ -1,8 +1,31 @@
 <?php
 
 /*
- * You can place your custom package configuration in here.
+ * This file is part of the Laravel Fluidcoins package.
+ *
+ * (c) Sanusi Mubaraq <mubaraqsanusi908@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
+
 return [
 
+    /**
+     * Public Key From Fluidcoins Dashboard
+     *
+     */
+    'publicKey' => getenv('FLUIDCOINS_PUBLIC_KEY'),
+
+    /**
+     * Secret Key From Fluidcoins Dashboard
+     *
+     */
+    'secretKey' => getenv('FLUIDCOINS_SECRET_KEY'),
+
+    /**
+     * Fluidcoins URL
+     *
+     */
+    'baseUrl' => getenv('FLUIDCOINS_URL'),
 ];
